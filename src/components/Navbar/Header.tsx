3525b5eb4d5576fe6a07bbc7b8afa835bede1dd3
gamitej <gamitejpratapsingh998@gmail.com>
2023-09-03 01:00:43 +0530

    header sticky

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -11,7 +11,7 @@ const Header = ({ navbarHeight }: AppAuth) => {
 
   return (
     <div
-      className={`bg-darkCard shadow-md flex justify-between items-center px-5`}
+      className={`sticky top-0 z-[100] bg-darkCard shadow-md flex justify-between items-center px-5`}
       style={{ height: `${navbarHeight}rem` }}
     >
       <div className="flex items-center gap-x-4">
